f59d0ee2b8a3c29d85ddb0116e136d03abc1d772
priyakhot64 <priyakhot64@gmail.com>
2021-01-27 10:48:37 +0530

    var2 added

--- src/main/java/com/ashokit/App3Application.java
+++ src/main/java/com/ashokit/App3Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App3Application {
+	int b = 20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(App3Application.class, args);
